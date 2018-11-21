c12781c29c71a3ba869a3d450bfecb5121c7d4fd
Lucas CORDENOD <lcordeno@e3r12p5.42.fr>
2018-11-21 18:10:24 +0100

    second commit

---
+++ get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcordeno <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/21 13:53:20 by lcordeno          #+#    #+#             */
+/*   Updated: 2018/11/21 16:40:33 by lcordeno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#define BUFF_SIZE 32
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "../libft/libft.h"
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+/*typedef struct	s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}				t_list;
+*/
+int		get_next_line(const int fd, char **line);
+
+#endif
